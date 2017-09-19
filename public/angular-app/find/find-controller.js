56a6038d22549a53711280e51e1a78bbdc92a551
Edward Dupre <edwd42@gmail.com>
2017-09-19 23:37:08 +0000

    added vm.error = ''; clears error from page, find-controller.js:13

--- public/angular-app/find/find-controller.js
+++ public/angular-app/find/find-controller.js
@@ -2,18 +2,21 @@ angular.module('cdfinance').controller("FindController", FindController);
 
 function FindController($http) {
   var vm = this;
-  console.log("findController");
+  console.log("findController 5");
   vm.find = function() {
-    var symbol = vm.symbol
-    console.log(symbol)
+    var symbol = vm.symbol;
+    console.log("8", symbol);
     
-    $http.get("/api/stocks/" + symbol).then(function(response) {
-      console.log("found stock")
-      var stockprice = response.data.price
-      vm.stockprice = stockprice;
+    $http.get("/api/stocks/" + symbol)
+      .then(function(response) {
+        console.log("found stock")
+        // vm.error = ''; clears error from page 13: find-controller.js
+        vm.error = '';
+        var stockprice = response.data.price
+        vm.stockprice = stockprice;
     }).catch(function(error) {
       if (error) {
-        vm.error = error;
+        vm.error = "Stock not found";
       }
     })
   }
