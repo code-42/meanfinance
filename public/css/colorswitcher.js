f926731522e68bda2041cc6f3d3c5b57dc6a84f0
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-09-21 01:26:49 +0000

    updated toggle for night theme button

--- public/css/colorswitcher.js
+++ public/css/colorswitcher.js
@@ -1,6 +1,16 @@
-document.getElementById('switch-color').onclick = switchNight; //this is the button id name
+// document.getElementById('switch-color').onclick = switchNight; //this is the button id name
 
-function switchNight(){
-    document.getElementsByTagName('body')[0].style.backgroundColor = 'gray'; 
-    document.getElementsByClassName('night')[0].style.color = 'white';
-};
+// function switchNight(){
+//     document.getElementsByTagName('body')[0].style.backgroundColor = 'gray'; 
+//     document.getElementsByClassName('night')[0].style.color = 'white';
+// };
+
+$('#switch-color').click(function(){
+  $('#body-toggle').toggleClass('night');
+    $('#switch-color').text(function    (i, v){
+        return v === 'Day Mode' ? 'Night Mode' : 'Day Mode'
+    });
+        $('#switch-color').toggleClass('daymode-button');
+});
+
+//better to toggle visibility of existing?
